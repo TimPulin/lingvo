a6176854037208dcea79315d47dd4bd64267244f
gen <kyliki@yandex.ru>
2023-08-26 01:36:55 +0300

    animation forward realised

--- src/site/HeaderSite.tsx
+++ src/site/HeaderSite.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
 import { RootStateType } from '../store';
 import { getCurrentLangPack } from '../utils/lang-pack/get-current-lang-pack';
 
@@ -7,10 +8,23 @@ export default function HeaderSite() {
   const currentLang = useSelector((store: RootStateType) => store.currentLang.value);
   const { SETTINGS } = getCurrentLangPack({ langCode: currentLang });
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isBurgerTransform, setIsBurgerTransform] = useState(false);
+
+  function handleBurgerClick() {
+    setIsBurgerTransform(true);
+    setTimeout(() => {
+      setIsMenuOpen(true);
+    }, 500);
+  }
+
+  const burgerAnimationClasses = () => (isBurgerTransform ? 'burger--vertical-line' : '');
+  const navOperateClass = () => (isMenuOpen ? 'nav__panel--open' : '');
+
   return (
     <header>
       <nav className="nav">
-        <div className="nav__panel">
+        <div className={`nav__panel ${navOperateClass()}`}>
           <ul className="nav__list">
             <li className="nav__item">
               <Link className="nav__link" to="/settings">{SETTINGS}</Link>
@@ -21,10 +35,10 @@ export default function HeaderSite() {
         <div className="burger">
           <button
             type="button"
-            className="burger__btn"
+            className={`burger__btn ${burgerAnimationClasses()}`}
             aria-label="open-navigation"
+            onClick={() => handleBurgerClick()}
           >
-            <span className="burger__line" />
             <span className="burger__line" />
             <span className="burger__line" />
             <span className="burger__line" />
