ea4a7579a661247b2b9dd1ff59539762f4d90d29
gen <kyliki@yandex.ru>
2023-10-25 00:33:13 +0300

    logout realised

--- src/components/user/UserBlock.tsx
+++ src/components/user/UserBlock.tsx
@@ -1,10 +1,17 @@
 import { useEffect, useState } from 'react';
+
 import { getUserData } from '../../connect/server-connections';
 import { useUserToken } from '../../store/selectors';
+import ButtonBase from '../base/ButtonBase';
+import ExitIcon from '../icons/ExitIcon';
 
 const IMG_PATH_DEFAULT = '/images/icons/icon-user-default.png';
 
-export default function UserBlock() {
+type UserBlockPropsType = {
+  onLogout: () => void;
+};
+
+export default function UserBlock(props:UserBlockPropsType) {
   const userToken = useUserToken();
   const [userName, setUserName] = useState('');
   const [userAvatar, setUserAvatar] = useState(IMG_PATH_DEFAULT);
@@ -24,6 +31,12 @@ export default function UserBlock() {
 
   return (
     <div className="user">
+      <span className="user__logout-block">
+        <ButtonBase
+          onClickFunction={props.onLogout}
+          ElementJSX={<ExitIcon />}
+        />
+      </span>
       <div className="user__icon-block">
         <img className="user__img" src={userAvatar} alt="user avatar" />
       </div>
