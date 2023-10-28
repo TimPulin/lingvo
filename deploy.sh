ec107846d3f0c75f942480c645d10d80d49dba51
Pavel Pulin <pavel.pulin@smile2impress.com>
2023-10-28 12:08:00 +0300

    add clean cahce

--- deploy.sh
+++ deploy.sh
@@ -9,3 +9,7 @@ echo $PATH_NAME
 echo $PROFILE_NAME
 
 aws s3 --profile $PROFILE_NAME cp $PATH_NAME s3://lingvocards.space/ --recursive
+
+aws cloudfront create-invalidation \
+    --distribution-id E3UIW9XS7KUS57 \
+    --paths "/*"
